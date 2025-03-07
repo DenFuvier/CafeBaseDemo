b50f88c32d46242441d01a94fa5a1e4691e163ad
Den Fuvier <findalone@protonmail.com>
2025-03-07 18:31:39 +0300

    V 6.0

--- Windows/AddShifts.Designer.cs
+++ Windows/AddShifts.Designer.cs
@@ -37,89 +37,122 @@
             this.Data_Box = new System.Windows.Forms.TextBox();
             this.Time_Box = new System.Windows.Forms.TextBox();
             this.exit = new System.Windows.Forms.Button();
+            this.Upload = new System.Windows.Forms.Button();
+            this.ViewUpload = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.UserIDView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ViewUpload)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(26, 258);
+            this.label1.Location = new System.Drawing.Point(11, 216);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(153, 16);
+            this.label1.Size = new System.Drawing.Size(107, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "UserdID пользователя";
+            this.label1.Text = "UserdID сотрудника";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(26, 286);
+            this.label2.Location = new System.Drawing.Point(11, 238);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.Size = new System.Drawing.Size(33, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.label2.Text = "Дата";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(26, 314);
+            this.label3.Location = new System.Drawing.Point(11, 261);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.Size = new System.Drawing.Size(40, 13);
             this.label3.TabIndex = 2;
-            this.label3.Text = "label3";
+            this.label3.Text = "Время";
             // 
             // UserIDView
             // 
             this.UserIDView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.UserIDView.Location = new System.Drawing.Point(12, 12);
+            this.UserIDView.Location = new System.Drawing.Point(9, 10);
+            this.UserIDView.Margin = new System.Windows.Forms.Padding(2);
             this.UserIDView.Name = "UserIDView";
+            this.UserIDView.ReadOnly = true;
             this.UserIDView.RowHeadersWidth = 51;
             this.UserIDView.RowTemplate.Height = 24;
-            this.UserIDView.Size = new System.Drawing.Size(776, 150);
+            this.UserIDView.Size = new System.Drawing.Size(661, 94);
             this.UserIDView.TabIndex = 4;
             // 
             // ID_Box
             // 
-            this.ID_Box.Location = new System.Drawing.Point(-4, 0);
+            this.ID_Box.Location = new System.Drawing.Point(-3, 0);
+            this.ID_Box.Margin = new System.Windows.Forms.Padding(2);
             this.ID_Box.Name = "ID_Box";
-            this.ID_Box.Size = new System.Drawing.Size(10, 22);
+            this.ID_Box.Size = new System.Drawing.Size(8, 20);
             this.ID_Box.TabIndex = 5;
             // 
             // UserID_Box
             // 
-            this.UserID_Box.Location = new System.Drawing.Point(89, 252);
+            this.UserID_Box.Location = new System.Drawing.Point(122, 211);
+            this.UserID_Box.Margin = new System.Windows.Forms.Padding(2);
             this.UserID_Box.Name = "UserID_Box";
-            this.UserID_Box.Size = new System.Drawing.Size(100, 22);
+            this.UserID_Box.Size = new System.Drawing.Size(170, 20);
             this.UserID_Box.TabIndex = 6;
             // 
             // Data_Box
             // 
-            this.Data_Box.Location = new System.Drawing.Point(89, 280);
+            this.Data_Box.Location = new System.Drawing.Point(122, 236);
+            this.Data_Box.Margin = new System.Windows.Forms.Padding(2);
             this.Data_Box.Name = "Data_Box";
-            this.Data_Box.Size = new System.Drawing.Size(100, 22);
+            this.Data_Box.Size = new System.Drawing.Size(170, 20);
             this.Data_Box.TabIndex = 7;
             // 
             // Time_Box
             // 
-            this.Time_Box.Location = new System.Drawing.Point(89, 308);
+            this.Time_Box.Location = new System.Drawing.Point(122, 258);
+            this.Time_Box.Margin = new System.Windows.Forms.Padding(2);
             this.Time_Box.Name = "Time_Box";
-            this.Time_Box.Size = new System.Drawing.Size(100, 22);
+            this.Time_Box.Size = new System.Drawing.Size(170, 20);
             this.Time_Box.TabIndex = 8;
             // 
             // exit
             // 
-            this.exit.Location = new System.Drawing.Point(674, 403);
+            this.exit.Location = new System.Drawing.Point(575, 281);
+            this.exit.Margin = new System.Windows.Forms.Padding(2);
             this.exit.Name = "exit";
-            this.exit.Size = new System.Drawing.Size(114, 35);
+            this.exit.Size = new System.Drawing.Size(86, 28);
             this.exit.TabIndex = 9;
             this.exit.Text = "Вернуться";
             this.exit.UseVisualStyleBackColor = true;
             this.exit.Click += new System.EventHandler(this.exit_Click);
             // 
+            // Upload
+            // 
+            this.Upload.Location = new System.Drawing.Point(297, 211);
+            this.Upload.Name = "Upload";
+            this.Upload.Size = new System.Drawing.Size(106, 30);
+            this.Upload.TabIndex = 10;
+            this.Upload.Text = "Добавить";
+            this.Upload.UseVisualStyleBackColor = true;
+            this.Upload.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ViewUpload
+            // 
+            this.ViewUpload.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ViewUpload.Location = new System.Drawing.Point(9, 109);
+            this.ViewUpload.Name = "ViewUpload";
+            this.ViewUpload.Size = new System.Drawing.Size(661, 94);
+            this.ViewUpload.TabIndex = 11;
+            // 
             // AddShifts
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(672, 320);
+            this.Controls.Add(this.ViewUpload);
+            this.Controls.Add(this.Upload);
             this.Controls.Add(this.exit);
             this.Controls.Add(this.Time_Box);
             this.Controls.Add(this.Data_Box);
@@ -129,10 +162,12 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "AddShifts";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "AddShifts";
             ((System.ComponentModel.ISupportInitialize)(this.UserIDView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ViewUpload)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -149,5 +184,7 @@
         private System.Windows.Forms.TextBox Data_Box;
         private System.Windows.Forms.TextBox Time_Box;
         private System.Windows.Forms.Button exit;
+        private System.Windows.Forms.Button Upload;
+        private System.Windows.Forms.DataGridView ViewUpload;
     }
 }
--- Windows/AddShifts.cs
+++ Windows/AddShifts.cs
@@ -4,13 +4,9 @@ using CafeBase.Trash;
 using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace CafeBase.Windows
 {
@@ -18,10 +14,65 @@ namespace CafeBase.Windows
     {
         SqlConnector sql = new SqlConnector();
         List<Users> use_ = new List<Users>();
+        List<SShifts> shifts_ = new List<SShifts>();
         public AddShifts()
         {
             InitializeComponent();
             UserIDKiad();
+            LoadPOOP();
+            UserIDView.SelectionChanged += UserIDView_SelectionChanged;
+            ViewUpload.SelectionChanged += ViewUpload_SelectionChanged;
+        }
+        private void UserIDView_SelectionChanged(object sender, EventArgs e)
+        { 
+         int number = UserIDView.CurrentRow.Index;
+            Users X = use_[number];
+            UserID_Box.Text = X.id.ToString();
+        }
+        private void ViewUpload_SelectionChanged(object sender, EventArgs e)
+        { 
+         int numbers = ViewUpload.CurrentRow.Index;
+         SShifts Y = shifts_[numbers];
+         Data_Box.Text = Y.ShiftsDate.ToString("yyyy-MM-dd");
+         Time_Box.Text = Y.Shiftstime.ToString(@"hh\:mm\:ss");
+        }
+        private void LoadPOOP()
+        {
+            string cs = sql.Getconnect();
+            try
+            {
+             var con = new MySqlConnection(cs);
+             con.Open();
+             var stm = "SELECT id , Userid , ShiftsDate , Shiftstime FROM shifts";
+             var cmd = new MySqlCommand(stm , con);
+             MySqlDataReader reader = cmd.ExecuteReader();
+             shifts_.Clear();
+             while (reader.Read())
+             {
+               int id = reader.GetInt32(0);
+               int Userid = reader.GetInt32(1);
+               DateTime ShiftsDate = reader.GetDateTime(2);
+               TimeSpan Shiftstime = reader.GetTimeSpan(3);
+
+                    SShifts sShifts_ = new SShifts
+                    {
+
+                        id = id,
+                        Userid = Userid,
+                        ShiftsDate = ShiftsDate,
+                        Shiftstime = Shiftstime
+                    };
+                    shifts_.Add(sShifts_);
+             }
+                ViewUpload.DataSource = null;
+                ViewUpload.DataSource = shifts_;
+                ViewUpload.Refresh();
+                con.Close();
+            }
+            catch(Exception ex)
+            {
+             MessageBox.Show(ex.Message);
+            }
         }
         private void UserIDKiad()
         {
@@ -33,6 +84,7 @@ namespace CafeBase.Windows
                 string stm = "SELECT id , name , surname FROM access_rights";
                 var cmd = new MySqlCommand(stm, con);
                 MySqlDataReader reader = cmd.ExecuteReader();
+                use_.Clear();
                 while (reader.Read())
                 {
                     int id = reader.GetInt32(0);
@@ -51,6 +103,7 @@ namespace CafeBase.Windows
                 UserIDView.DataSource = use_;
                 UserIDView.Columns["Job_title"].Visible = false;
                 UserIDView.Columns["Status"].Visible = false;
+                ViewUpload.Refresh();
             }
             catch (Exception ex)
             {
@@ -67,5 +120,52 @@ namespace CafeBase.Windows
             sda.Show();
 
         }
+        private void AdddButton()
+        {
+            string cs = sql.Getconnect();
+            try
+            {
+                var con = new MySqlConnection(cs);
+                con.Open();
+                var stm = $"INSERT INTO shifts (UserID , ShiftsDate ,Shiftstime ) VALUES ('{UserID_Box.Text}' , '{Data_Box.Text}' , '{Time_Box.Text}') ";
+                var cmd = new MySqlCommand(stm, con);
+                MySqlDataReader Reader = cmd.ExecuteReader();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+      /*  private void change()
+        {
+
+            string cs = sql.Getconnect();
+            try
+            {
+                var con = new MySqlConnection(cs);
+                con.Open();
+                var stm = $"UPDATE shifts SET ShiftsDate = '{Data_Box.Text}', Shiftstime = '{Time_Box.Text}' WHERE UserID = '{UserID_Box.Text}'";
+                var cmd = new MySqlCommand(stm, con);
+                MySqlDataReader Reader = cmd.ExecuteReader();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+      */
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AdddButton();
+            LoadPOOP();
+        }
+
+        //private void Chage_b_Click(object sender, EventArgs e)
+        //{
+          //  change();
+            //LoadPOOP();
+        //}
     }
 }
